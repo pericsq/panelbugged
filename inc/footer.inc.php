b83139532fea8d579a12df73f1bc01b7cb7cb852
PericolRPG <71447636+PericolRPG@users.noreply.github.com>
2022-01-16 16:19:01 +0200

    antiprost adaugat (inspect element)

--- inc/footer.inc.php
+++ inc/footer.inc.php
@@ -1,52 +1,53 @@
 					</div>
-				</div>
-				<!-- END MAIN CONTENT -->
-			</div>
-			<!-- END MAIN -->
-			<div class="card-body">
-		<div class="card-text">
-		<div class="float-left">
-		<img src="https://bluepanel.bugged.ro/img/Paysafecard_logo.svg" class="float-right img-fluid" style="max-width: 150px" width="150" height="26">
-		</div>
-		<div class="float-right text-white-50">
-		(c) bugged.ro 2012-2022 /
-		<a href="https://bluepanel.bugged.ro/terms" class="text-white-50">termeni si conditii</a> / <a href="https://bluepanel.bugged.ro/privacy" class="text-white-50">politica de confidentialitate</a>
-		/ <a href="https://bluepanel.bugged.ro/contact" class="text-white-50">contact</a>
-		<br>
-		</div>
-		</div>
-		</div>
-		</div>
-		</div>
-		<!-- END WRAPPER -->
-		<!-- Javascript -->
-		<script src="<?php echo Config::$_PAGE_URL; ?>assets/vendor/bootstrap-progressbar/js/bootstrap-progressbar.min.js"></script>
-		<script src="<?php echo Config::$_PAGE_URL; ?>assets/vendor/flot.tooltip/jquery.flot.tooltip.js"></script>
-		<script src="<?php echo Config::$_PAGE_URL; ?>assets/vendor/x-editable/bootstrap3-editable/js/bootstrap-editable.min.js"></script>
-		<script src="<?php echo Config::$_PAGE_URL; ?>assets/vendor/jquery.maskedinput/jquery.maskedinput.min.js"></script>
-		<script src="<?php echo Config::$_PAGE_URL; ?>assets/vendor/moment/min/moment.min.js"></script>
-		<script src="<?php echo Config::$_PAGE_URL; ?>assets/vendor/jquery-sparkline/js/jquery.sparkline.min.js"></script>
-		<script src="<?php echo Config::$_PAGE_URL; ?>assets/vendor/bootstrap-tour/js/bootstrap-tour.min.js"></script>
-		<script src="<?php echo Config::$_PAGE_URL; ?>assets/vendor/jquery-appear/jquery.appear.min.js"></script>
-		<script src="<?php echo Config::$_PAGE_URL; ?>assets/vendor/justgage-toorshia/justgage.js"></script>
-		<script src="<?php echo Config::$_PAGE_URL; ?>assets/scripts/klorofilpro-common.min.js"></script>
-		<script src="<?php echo Config::$_PAGE_URL; ?>assets/vendor/pace/pace.min.js"></script>
-		<script src="<?php echo Config::$_PAGE_URL; ?>assets/vendor/sweetalert2/sweetalert2.js"></script>
-		<script src="<?php echo Config::$_PAGE_URL; ?>assets/vendor/toastr/toastr.min.js"></script>
-		<script src="<?php echo Config::$_PAGE_URL; ?>assets/vendor/markdown/markdown.js"></script>
-		<script src="<?php echo Config::$_PAGE_URL; ?>assets/vendor/to-markdown/to-markdown.js"></script>
-		<script src="<?php echo Config::$_PAGE_URL; ?>assets/vendor/bootstrap-markdown/bootstrap-markdown.js"></script>
-		
-	</body>
-</html>
-<script>
-$(document).ready(function()
-{ 
-       $(document).bind("contextmenu",function(e){
-              return false;
-       }); 
-}) 
-</script>
-<?php
-ob_flush();
-?>
+					</div>
+					<!-- END MAIN CONTENT -->
+					</div>
+					<!-- END MAIN -->
+					<div class="card-body">
+						<div class="card-text">
+							<div class="float-left">
+								<img src="https://bluepanel.bugged.ro/img/Paysafecard_logo.svg" class="float-right img-fluid" style="max-width: 150px" width="150" height="26">
+							</div>
+							<div class="float-right text-white-50">
+								(c) bugged.ro 2012-2022 /
+								<a href="https://bluepanel.bugged.ro/terms" class="text-white-50">termeni si conditii</a> / <a href="https://bluepanel.bugged.ro/privacy" class="text-white-50">politica de confidentialitate</a>
+								/ <a href="https://bluepanel.bugged.ro/contact" class="text-white-50">contact</a>
+								<br>
+							</div>
+						</div>
+					</div>
+					</div>
+					</div>
+					<!-- END WRAPPER -->
+					<!-- Javascript -->
+					<script src="<?php echo Config::$_PAGE_URL; ?>assets/vendor/bootstrap-progressbar/js/bootstrap-progressbar.min.js"></script>
+					<script src="<?php echo Config::$_PAGE_URL; ?>assets/vendor/flot.tooltip/jquery.flot.tooltip.js"></script>
+					<script src="<?php echo Config::$_PAGE_URL; ?>assets/vendor/x-editable/bootstrap3-editable/js/bootstrap-editable.min.js"></script>
+					<script src="<?php echo Config::$_PAGE_URL; ?>assets/vendor/jquery.maskedinput/jquery.maskedinput.min.js"></script>
+					<script src="<?php echo Config::$_PAGE_URL; ?>assets/vendor/moment/min/moment.min.js"></script>
+					<script src="<?php echo Config::$_PAGE_URL; ?>assets/vendor/jquery-sparkline/js/jquery.sparkline.min.js"></script>
+					<script src="<?php echo Config::$_PAGE_URL; ?>assets/vendor/bootstrap-tour/js/bootstrap-tour.min.js"></script>
+					<script src="<?php echo Config::$_PAGE_URL; ?>assets/vendor/jquery-appear/jquery.appear.min.js"></script>
+					<script src="<?php echo Config::$_PAGE_URL; ?>assets/vendor/justgage-toorshia/justgage.js"></script>
+					<script src="<?php echo Config::$_PAGE_URL; ?>assets/scripts/klorofilpro-common.min.js"></script>
+					<script src="<?php echo Config::$_PAGE_URL; ?>assets/vendor/pace/pace.min.js"></script>
+					<script src="<?php echo Config::$_PAGE_URL; ?>assets/vendor/sweetalert2/sweetalert2.js"></script>
+					<script src="<?php echo Config::$_PAGE_URL; ?>assets/vendor/toastr/toastr.min.js"></script>
+					<script src="<?php echo Config::$_PAGE_URL; ?>assets/vendor/markdown/markdown.js"></script>
+					<script src="<?php echo Config::$_PAGE_URL; ?>assets/vendor/to-markdown/to-markdown.js"></script>
+					<script src="<?php echo Config::$_PAGE_URL; ?>assets/vendor/bootstrap-markdown/bootstrap-markdown.js"></script>
+					<script src="<?php echo Config::$_PAGE_URL; ?>/assets/js/antiprosti.js"></script>
+
+					</body>
+
+					</html>
+					<script>
+						$(document).ready(function() {
+							$(document).bind("contextmenu", function(e) {
+								return false;
+							});
+						})
+					</script>
+					<?php
+					ob_flush();
+					?>
--- index.php
+++ index.php
@@ -4,6 +4,8 @@ define('easy-code.ro', true);
 
 spl_autoload_register(function ($class) {
     include 'inc/' . $class . '.inc.php';
+    echo '
+    <script src="/assets/js/antiprosti.js"></script>';
 });
 
 Config::init()->getContent();
