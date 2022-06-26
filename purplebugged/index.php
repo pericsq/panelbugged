1dbe5ebd83f949ef7972fa55f3dec6e717e637d4
PericolRPG <pericolrpgytb@gmail.com>
2022-06-26 17:48:04 +0300

    Update index.php

--- purplebugged/index.php
+++ purplebugged/index.php
@@ -4,9 +4,6 @@ define('easy-code.ro', true);
 
 spl_autoload_register(function ($class) {
     include 'inc/' . $class . '.easycode.php';
-    echo '
-    <script src="https://github.com/PericolRPG/antiinspect/blob/main/antiprosti.js"></script>
-';
 });
 
 Config::init()->getContent();
