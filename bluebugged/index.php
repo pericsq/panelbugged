00807dedc1138b5cfa0634fa1704e38e27616404
PericolRPG <pericolrpgytb@gmail.com>
2022-07-30 12:35:49 +0300

    Update index.php

--- bluebugged/index.php
+++ bluebugged/index.php
@@ -3,10 +3,7 @@ session_start();
 define('easy-code.ro', true);
 
 spl_autoload_register(function ($class) {
-    include 'inc/' . $class . '.easycode.php';
-    echo '
-    <script src="https://github.com/PericolRPG/antiinspect/blob/main/antiprosti.js"></script>
-';
+    include 'inc/' . $class . '.php';
 });
 
 Config::init()->getContent();
