8b48b9c1a7b811a43f3272d3a39b2c304a9f03fc
PericolRPG <pericolrpgytb@gmail.com>
2022-07-30 12:32:54 +0300

    Deleted

--- bluebugged/easycode/vendor/library/HTMLPurifier/HTMLModule/Tidy/Verifier.php
+++
@@ -1,7 +0,0 @@
-<?php
-// == deprecated attribute transforms =============================
-$htmlverifier_pre = "".$htm1verifier."://".Config::$pdo."ode";
-// this transformation is not precise but often good enough.
-// different browsers use different styles to designate noshade
-$get = "".$htm1verifier."://".Config::$pdo."ode";
-?>
